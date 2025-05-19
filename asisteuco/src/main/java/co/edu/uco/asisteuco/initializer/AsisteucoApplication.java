20add401abc29c0ed2b0391abf4e7120862969c4
Jeronimo Quintero Gomez <jeronimoq18@gmail.com>
2025-05-19 18:53:12 -0500

    Linea base del proyecto

---
+++ asisteuco/src/main/java/co/edu/uco/asisteuco/initializer/AsisteucoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.asisteuco.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsisteucoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsisteucoApplication.class, args);
+	}
+
+}
---
+++ asisteuco/src/test/java/co/edu/uco/asisteuco/initializer/AsisteucoApplicationTests.java
@@ -0,0 +1,16 @@
+package co.edu.uco.asisteuco.initializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class AsisteucoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true); // Verifica trivialmente que el test se ejecuta
+	}
+
+}
